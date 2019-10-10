237c718b1cceafb03d9f4dedcc52e787b33136a5
Valeria <valeria.gutsuliak@gmail.com>
2019-10-10 12:57:41 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,8 @@
+class Backer
+    attr_reader :name, :backed_projects
+
+    def initialize(name, backed_projects=[] )
+        @name = name
+        @backed_projects = backed_projects
+    end 
+end 
